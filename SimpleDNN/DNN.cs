fa74130a6410fb83de1c740f49a33020f34e7d86
dariki1 <james_atkins97@hotmail.com>
2019-07-24 15:32:55 +1000

    Added start of DNN
    Added DNN, Node and Weight classes, functions and properties

---
+++ SimpleDNN/DNN.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDNN {
+	class DNN {
+		private Node[][] nodes;
+		private Weight[] weights;
+		public DNN(int inputNumber, int outputNumber, int[] hiddenNumber) : this(inputNumber, outputNumber) {
+			
+		}
+
+		public DNN(int inputNumber, int outputNumber) {
+			
+		}
+	}
+
+	class Node {
+		private double bias;
+		private double value;
+		private double output;
+		private ActivationType activator = ActivationType.Sigmoid;
+
+		public enum ActivationType {
+			Sigmoid,
+			Tanh
+		}
+
+		public Node(ActivationType activatorType) {
+			bias = 1;
+			value = 0;
+			output = 0;
+			activator = activatorType;
+		}
+
+		public double Activate(int inputTotal) {
+			value = inputTotal;
+			output = activator == ActivationType.Sigmoid ? sigmoid(value) : tanh(value);
+			return 0.0;
+		}
+
+		private static double sigmoid(double value) {
+			return 1 / (1 + Math.Exp(-1 * value));
+		}
+
+		private static double sigmoidDerivative(double value) {
+			return sigmoid(value) * (1 - sigmoid(value));
+		}
+
+		private static double tanh(double value) {
+			return Math.Tanh(value);
+		}
+
+		private static double tanhDerivative(double value) {
+			return 1 - Math.Pow(Math.Tanh(value), 2);
+		}
+	}
+	class Weight {
+		public int inputNodeIndex { get; }
+		public int outputNodeIndex { get; }
+		public Weight(int inputIndex, int outputIndex) {
+			inputNodeIndex = inputIndex;
+			outputNodeIndex = outputIndex;
+		}
+	}
+}
